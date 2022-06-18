a107775ae5cb55483a194ed2e9d10c635e25a551
Jakub <jkb.olbrycht@gmail.com>
2022-06-18 22:22:06 +0200

    Product Details, Categories & Seeding more products.

---
+++ BlazorEcommerce/Server/Services/ProductServicee/IProductService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEcommerce.Server.Services.ProductServicee
+{
+	public interface IProductService
+	{
+		Task<ServiceResponse<List<Product>>> GetProductsAsync();
+		Task<ServiceResponse<Product>> GetProductAsync(int productId);
+
+	}
+}
