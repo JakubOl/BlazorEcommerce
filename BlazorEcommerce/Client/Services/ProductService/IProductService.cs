7d6ff3e618e15748f4a625d16821dfcdf50c04c0
Jakub <jkb.olbrycht@gmail.com>
2022-06-19 22:57:11 +0200

    Added searching, variants, types.

--- BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -2,10 +2,15 @@
 {
 	public interface IProductService
 	{
+		event Action ProductsChanged;
 		List<Product> Products { get; set; }
 
-		Task GetProducts();
+		string Message { get; set; }
+
+		Task GetProducts(string? categoryUrl = null);
 
 		Task<ServiceResponse<Product>> GetProduct(int productId);
+		Task SearchProducts(string searchText);
+		Task<List<string>> GetProductSearchSuggestions(string searchText);
 	}
 }
--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -27,5 +27,26 @@ namespace BlazorEcommerce.Server.Controllers
             var result = await _productService.GetProductAsync(productId);
             return Ok(result);
         }
+
+        [HttpGet("category/{categoryUrl}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductsByCategory(string categoryUrl)
+        {
+            var result = await _productService.GetProductsByCategory(categoryUrl);
+            return Ok(result);
+        }
+
+        [HttpGet("search/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> SearchProducts(string searchText)
+        {
+            var result = await _productService.SearchProducts(searchText);
+            return Ok(result);
+        }
+
+        [HttpGet("searchsuggestion/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _productService.GetProductSearchSuggestions(searchText);
+            return Ok(result);
+        }
     }
 }
